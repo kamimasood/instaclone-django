cf303d87298fcea69b9837fcc959b8ac7bbbfbfc
kam.msod <kammasood12@gmail.com>
2026-02-06 23:07:40 -0600

    Add database url.

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=False,
+    default=os.environ.get("DATABASE_URL"),        
+    conn_max_age=600,
+    ssl_require=False,
     )
     
 }
