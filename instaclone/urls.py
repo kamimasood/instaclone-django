ef116077c665e3a085e0015e386bb53648f84071
kam.msod <kammasood12@gmail.com>
2026-02-06 22:56:27 -0600

    Sanity check route.

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,34 +1,44 @@
-"""
-URL configuration for instaclone project.
+# """
+# URL configuration for instaclone project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from posts import views as post_views
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from posts import views as post_views
+
+from django.http import HttpResponse
+from django.urls import path
+
+def sanity_check(request):
+    return HttpResponse("HEROKU DJANGO IS WORKING")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('feed/', post_views.feed, name='home'),
-    path('', include('accounts.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls', namespace='posts')),
-    path('interactions/', include('interactions.urls')),
-    path('chats/', include('chats.urls')),
-    path('notifications/', include('notifications.urls')),
+    path('', sanity_check),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('feed/', post_views.feed, name='home'),
+#     path('', include('accounts.urls')),
+#     path('accounts/', include('accounts.urls')),
+#     path('posts/', include('posts.urls', namespace='posts')),
+#     path('interactions/', include('interactions.urls')),
+#     path('chats/', include('chats.urls')),
+#     path('notifications/', include('notifications.urls')),
+# ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
